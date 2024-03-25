000000045738ef19e1e81949e5894a126f350bfa
Tdxdxoz <tdxdxoz@gmail.com>
2024-03-25 18:11:57 +0800

    support multi threads

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::fmt::Write;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use git2::{Commit, Repository, Signature};
 use sha1::{Digest, Sha1};
@@ -43,46 +45,68 @@ fn main() {
     let mut buffer = String::new();
     show_commit(&head_commit, &mut buffer).unwrap();
 
-    let [part1, part2] = buffer
-        .split("committer ")
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
-    for i in 0..usize::MAX {
-        let name_pre = format!("{:x}_", i);
-        let full = format!(
-            "commit {}\0{}committer {}{}",
-            buffer.len() + name_pre.len(),
-            part1,
-            name_pre,
-            part2
-        );
+    let answer = Arc::new(Mutex::new(None));
+    let t_num = num_cpus::get().min(16);
 
-        // println!("{}", full);
-
-        let mut hasher = Sha1::new();
-        hasher.update(full.as_bytes());
-        let result = hasher.finalize();
-        if result
-            .into_iter()
-            .map(|v| [v / 16, v % 16])
-            .flatten()
-            .take(7)
-            .all(|v| v == 0)
-        {
-            let committer = head_commit.committer();
-            let new_name = format!("{}{}", name_pre, committer.name().unwrap_or_default());
-            let new_committer = Signature::new(
-                &new_name,
-                committer.email().unwrap_or_default(),
-                &committer.when(),
-            )
-            .unwrap();
-            let oid = head_commit
-                .amend(Some("HEAD"), None, Some(&new_committer), None, None, None)
+    let mut handles = vec![];
+    for tid in 0..t_num {
+        let answer = answer.clone();
+        let orig = buffer.clone();
+        let h = thread::spawn(move || {
+            let [part1, part2] = orig
+                .split("committer ")
+                .collect::<Vec<_>>()
+                .try_into()
                 .unwrap();
-            println!("Done, new commit hash {}", oid);
-            break;
-        }
+            for i in 0..usize::MAX {
+                if answer.lock().unwrap().is_some() {
+                    break;
+                }
+
+                let name_pre = format!("{}{:x}_", (tid as u8 + 65) as char, i);
+                // println!("{}", name_pre);
+
+                let full = format!(
+                    "commit {}\0{}committer {}{}",
+                    orig.len() + name_pre.len(),
+                    part1,
+                    name_pre,
+                    part2
+                );
+                // println!("{}", full);
+
+                let mut hasher = Sha1::new();
+                hasher.update(full.as_bytes());
+                let result = hasher.finalize();
+                if result
+                    .into_iter()
+                    .map(|v| [v / 16, v % 16])
+                    .flatten()
+                    .take(7)
+                    .all(|v| v == 0)
+                {
+                    *answer.lock().unwrap() = Some(name_pre);
+                }
+            }
+        });
+        handles.push(h);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let name_pre = Arc::try_unwrap(answer)
+        .unwrap()
+        .into_inner()
+        .unwrap()
+        .expect("Can't found");
+    let committer = head_commit.committer();
+    let new_name = format!("{}{}", name_pre, committer.name().unwrap());
+    let new_committer =
+        Signature::new(&new_name, committer.email().unwrap(), &committer.when()).unwrap();
+    let oid = head_commit
+        .amend(Some("HEAD"), None, Some(&new_committer), None, None, None)
+        .unwrap();
+    println!("Done, new commit hash {}", oid);
 }
